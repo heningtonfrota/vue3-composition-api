881b1c87d39813242f05b23d1f5a0bc3a2785f06
Henington Frota <heningtonfrota@gmail.com>
2022-06-16 22:16:28 -0400

    ENTENDENDO UM COMPONENTE NO VUE 3

--- src/views/Todos/TodosView.vue
+++ src/views/Todos/TodosView.vue
@@ -1,11 +1,44 @@
 <template>
-    <div>
-        <h1>Lista de Tarefas</h1>
-    </div>
+    <h1>Lista de Tarefas</h1>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            {{ todo.name }}
+        </li>
+    </ul>
+
+    <input type="text" v-model="name">
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
-    name: 'TodosView'
+    name: 'TodosView',
+    setup() {
+        const todos = [
+            {
+                id: 1,
+                name: 'tarefa 01',
+                completed: true
+            },
+            {
+                id: 2,
+                name: 'tarefa 02',
+                completed: false
+            },
+            {
+                id: 3,
+                name: 'tarefa 03',
+                completed: true
+            },
+        ];
+
+        const name = ref('default value');
+
+        return {
+            todos,
+            name
+        }
+    }
 }
 </script>
